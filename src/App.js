70ccfae6ca2339b0feb88fc16eed2ef641f85a20
黄河金 <7432941+huanghejin@user.noreply.gitee.com>
2020-07-23 09:05:44 +0800

    首次部署项目结构

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <img className="login" src="http://localhost/login.jpg" />
+        <h1 style={{color:"#fff",marginLeft:"10px"}}>小熊官网</h1>
+      </div>
     </div>
   );
 }
